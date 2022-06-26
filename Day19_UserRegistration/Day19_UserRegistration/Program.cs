0484308275c13fc98ceeb4445112c076cdc8e763
shreegowri <gaganandh@gmail.com>
2022-06-26 18:11:40 +0530

    [shreegowri]validating the first name of the user

---
+++ Day19_UserRegistration/Day19_UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day19_UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //uc1
+            RegexClass regex = new RegexClass();
+            regex.ValidateFirstNameRegex("Shreegowri");
+                
+            
+        }
+    }
+}
---
+++ Day19_UserRegistration/Day19_UserRegistration/UserRegistration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_UserRegistration
+{
+    public class RegexClass
+    {
+        public Regex FirstNameRegex = new Regex(@"^[A-Z]{1}[A-Za-z]{3,}?$");
+        public void ValidateFirstNameRegex(string FirstName)
+        {
+            Console.WriteLine("\nFirstName:" + FirstName);
+            if(FirstNameRegex.IsMatch(FirstName))
+            {
+                Console.WriteLine("valide");
+            }
+            else
+            {
+                Console.WriteLine("not valid");
+            }
+            
+                    
+
+            
+
+
+        }
+        
+
+
+    }
+}
